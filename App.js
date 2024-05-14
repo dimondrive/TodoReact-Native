e23d58c205aa61c52390e7d4162dbd176847624e
дмитрий василевский <dmitrijvasilevskij@MacBook-Air-dmitrij.local>
2024-05-14 19:01:13 +0300

    Begin Todo

---
+++ App.js
@@ -0,0 +1,68 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,KeyboardAvoidingView, Platform,TextInput,TouchableOpacity,ScrollView, Keyboard} from 'react-native';
+import Task from './components/Task';
+import { useState } from 'react';
+
+export default function App() {
+  let date=new Date()
+  const [isFocus,setFocus]=useState(false);
+  const [task,setTask]=useState();
+  const [taskItems,setTaskItems]=useState([]);
+  const handkeAddTask=()=>{
+    Keyboard.dismiss();
+    setTaskItems([...taskItems,task]);
+    setTask(null)
+  }
+  const completeTask=(index)=>{
+    let itemsCopy=[...taskItems];
+    itemsCopy.splice(index,1);
+    setTaskItems(itemsCopy);
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.taskWrapper}>
+        <Text style={styles.title}>Today's tasks</Text>    
+        <View style={{position:'absolute',top:100,right:100}}><Text style={{position:'absolute'}}> {`${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`}</Text></View>       
+        <View style={styles.items}>
+          {taskItems.map((item,index)=>{
+            return (
+              <TouchableOpacity key={index} onPress={()=>completeTask(index)}><Task text={item}/></TouchableOpacity>
+            )
+          })}
+        </View>
+      </View>
+      <KeyboardAvoidingView behavior={Platform.OS==="ios"?'padding':'height'} style={styles.writeTaskWrapper}>
+        <TextInput placeholder='Задача' onBlur={()=>setFocus(false)} onFocus={()=>setFocus(true)} style={styles.input} value={task} onChangeText={text=>setTask(text)} />
+        <TouchableOpacity onPress={()=>handkeAddTask()}><View style={{width:60,height:60,borderRadius:52,backgroundColor:'white',justifyContent:'center',alignItems:'center',marginRight:29}}><Text style={{fontSize:35}}>+</Text></View></TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  taskWrapper:{
+    paddingTop:94,
+    paddingHorizontal:20,
+    paddingBottom:100,
+    position:'relative'
+  },
+title:{fontSize:24,
+fontWeight:'bold',marginBottom:30},
+items:{},
+input:{
+  backgroundColor:'white',
+  borderRadius:60,
+  paddingVertical:15,
+  paddingHorizontal:15,
+  width:246,
+},
+writeTaskWrapper:{position:'absolute',
+bottom:37,flexDirection:'row',
+alignItems:'center',paddingLeft:20,justifyContent:'space-between',width:'100%'},
+});
---
+++ components/Task.js
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
+import React from 'react'
+
+export default function Task({text}) {
+    let date =new Date();
+  return (
+    <>
+        <View style={styles.item}>
+        <View style={styles.itemLeft}>
+        <TouchableOpacity style={styles.square}></TouchableOpacity>
+        <Text style={styles.itemText}>{text}</Text>
+        </View>
+        <View style={styles.circular}></View>
+        <View style={{}}>
+        <Text style={{marginTop:15,textAlign:'right'}}>{`Время создания ${date.toLocaleDateString()} ${date.toLocaleTimeString()} ${date.getSeconds()}`}</Text>
+        </View>
+    </View>
+    
+    </>
+    
+  )
+}
+
+const styles = StyleSheet.create({
+    item:{
+        padding:15,
+        borderRadius:10,
+        backgroundColor:"white",
+        justifyContent:'center', 
+        marginBottom:20,
+        
+        
+    },
+    itemLeft:{
+        flexDirection:'row',
+        alignItems:'center',
+        flexWrap:'wrap',
+        position:'relative'
+        
+    },
+    circular:{
+        width:12,
+        height:12,
+        borderRadius:5,
+        borderWidth:2,
+        borderColor:'#55BCF6',
+        position:'absolute',
+        right:15,
+        top:25
+    },
+    square:{
+        width:24,
+        height:24,
+        borderRadius:5,
+        backgroundColor:'#55BCF666',
+        marginRight:15,
+    },
+    itemText:{
+        fontWeight:'bold',
+        maxWidth:'80%'
+    }
+})
